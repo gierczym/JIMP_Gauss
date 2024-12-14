9482f7517bd996048121c9fa4c217ba86b3516e4
Michal <michal.gierczynski@pw.edu.pl>
2024-12-14 21:23:22 +0100

    praca w toku ...

--- src/main.c
+++ src/main.c
@@ -3,28 +3,45 @@
 #include "mat_io.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 int main(int argc, char ** argv) {
-	int res;
 	Matrix * A = readFromFile(argv[1]);
 	Matrix * b = readFromFile(argv[2]);
 	Matrix * x;
+	
+	int flag_test = 0; // set to 1 when program called with -t flag as third argument
 
-	if (A == NULL) return -1;
-	if (b == NULL) return -2;
+	if( (argc > 3) && (0 == strcmp("-t", argv[3])) )
+		flag_test = 1;
+
+
+	if( flag_test ) {
+		printf( "\n=====================================================\n" );
+		printf( "TEST: Plik A: \"%s\", Plik b: \"%s\"\n", argv[1], argv[2] );
+	}
+
+	if (A == NULL) {
+		if( flag_test )
+			printf( "[!] FAILED: nie udalo sie wczytac macierzy A\n");
+		return -1;
+	}
+	if (b == NULL) {
+	       	return -2;
+	}
 	printf( "Wczytane dane:\n" );
 	printToScreen(A);
 	printToScreen(b);
 
-	res = eliminate(A,b);
+	eliminate(A,b);
 
 	x = createMatrix(b->r, 1);
 	if (x != NULL) {
 		printf( "\nWyniki:\n");
 		printToScreen(A);
 		printToScreen(b);
-		res = backsubst(x,A,b);
+		backsubst(x,A,b);
 		printToScreen(x);
 		freeMatrix(x);
 	} else {
