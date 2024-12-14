800f9386ff8129870bf018a300be1d4e62531b52
bolo <illuminati.kox2@gmail.com>
2024-12-14 17:12:48 +0100

    dodanie funkcjonalności zamieniania wierszy jeśli na diagonali jest zero, usunięcie funkcjonalności tworzenia macierzy diagonalnej i fetchowania wyników

--- src/gauss.c
+++ src/gauss.c
@@ -2,6 +2,7 @@
 #include "mat_io.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -12,36 +13,42 @@ void norm_row(Matrix *mat, Matrix *b, int row2norm, int row2benorm, double norma
 	for(int i = 0; i < mat -> c; i++){
 		mat -> data[row2benorm][i] += normaliser * mat -> data[row2norm][i];
 	}
-	
 	b -> data[row2benorm][0] += normaliser * b -> data[row2norm][0];
 }
-void fetch_results(Matrix *mat, Matrix *b){
-	for(int i = 0; i < mat -> c; i++){
-		b -> data[i][0] = b -> data[i][0] / mat -> data[i][i];
-	}
+
+void replace_rows(Matrix *mat, int r1, int r2){
+	double *tmp = mat -> data[r1];
+	mat -> data[r1] = mat -> data[r2];
+	mat -> data[r2] = tmp;
 }
-int is_matrix_singular(Matrix *mat){
-	if(mat -> r != mat -> c) //sprawdza, czy macierz jest kwadratowa
-		return 1;
-	for(int i = 0; i < mat -> c; i++){ //sprawdza, czy na diagonali jest 0
-		if(mat -> data[i][i] == 0)
-			return 1;
+
+int select_master_elem(Matrix *A, Matrix *b, int col){
+	int max_elem_row_index = -1;
+	double max_elem_val = 0;
+	for(int i = col; i < A -> r; i++){
+		if(fabs(A -> data[i][col]) > max_elem_val){
+			max_elem_val = fabs(A -> data[i][col]);
+			max_elem_row_index = i;
+		}
 	}
+	if(max_elem_val == 0){
+		printf("[!] select_master_elem: wszystkie wiersze w kolumnie %d od diagonali w dół mają współczynniki równe zero\n", col);
+		return 1;
+	}
+	replace_rows(A, col, max_elem_row_index);
+	replace_rows(b, col, max_elem_row_index);
 	return 0;
 }
 
 int eliminate(Matrix *mat, Matrix *b){
 	double dodajnik = 0;
-	if(is_matrix_singular(mat) == 1)
-		return 1;
 	for(int i = 0; i < mat -> c; i++){ // zamienia macierz na dolnotrójkątną
+		if(select_master_elem(mat, b, i) == 1)
+			return 1;
 		for(int j = i+1; j < mat -> r; j++){
 			dodajnik = -1 * (mat -> data[j][i]/mat -> data[i][i]);
 			norm_row(mat, b, i, j, dodajnik);
 		}
 	}
-	fetch_results(mat, b);
 	return 0;
 }
-
-
--- src/gauss.h
+++ src/gauss.h
@@ -11,8 +11,8 @@ int eliminate(Matrix *mat, Matrix *b);
 
 void norm_row(Matrix *mat, Matrix *b, int row2norm, int row2benorm, double normaliser); //normalizuje wiersz row2benorm za pomocą wiersza row2norm poprzez policzony wcześniej normalizer dla macierzy mat i b
 
-void fetch_results(Matrix *mat, Matrix *b); //dzieli każdy element macierzy wyników b przez odpowiadający jej element macierzy diagonalnej A
+void replace_rows(Matrix *mat, int r1, int r2); //zamienia w macierzy mat wiersz r1 z r2 (wskaźniki na nie)
 
-int is_matrix_singular(Matrix *mat); //sprawdza, czy macierz jest kwadratowa i czy na diagonali nie leży 0
+int select_master_elem(Matrix *A, Matrix *b, int col); //wybiera maksymalny moduł elementów kolumny macierzy, zwraca 0 jeśli na diagonali i poniżej są tylko zera
 
 #endif
