b8af45265e607dd05a11f3dd3825d06cf3cf4d60
Michal <michal.gierczynski@pw.edu.pl>
2024-12-14 21:53:31 +0100

    Poczatki funkcjonalnosci testowej. Plik Makefile ma juz trzy testy.

--- Makefile
+++ Makefile
@@ -2,4 +2,6 @@ all:
 	gcc -Wall --pedantic src/main.c src/mat_io.c src/gauss.c src/backsubst.c -o bin/gauss
 
 test: all
-	bin/gauss dane/A dane/b -t
+	@bin/gauss dane/A dane/b -t
+	@bin/gauss dane/A2 dane/b2 -t
+	@bin/gauss dane/A_niepopr1 dane/b_niepopr1 -t
--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 
 
 int main(int argc, char ** argv) {
+	int res;
 	Matrix * A = readFromFile(argv[1]);
 	Matrix * b = readFromFile(argv[2]);
 	Matrix * x;
@@ -28,24 +29,54 @@ int main(int argc, char ** argv) {
 		return -1;
 	}
 	if (b == NULL) {
+		if( flag_test )
+			printf( "[!] FAILED: nie udalo sie wczytac macierzy b\n");
 	       	return -2;
 	}
-	printf( "Wczytane dane:\n" );
-	printToScreen(A);
-	printToScreen(b);
 
-	eliminate(A,b);
-
-	x = createMatrix(b->r, 1);
-	if (x != NULL) {
-		printf( "\nWyniki:\n");
+	if( !flag_test ) {
+		printf( "Wczytane dane:\n" );
 		printToScreen(A);
 		printToScreen(b);
-		backsubst(x,A,b);
-		printToScreen(x);
-		freeMatrix(x);
+	}
+
+	if( !flag_test ) {
+		res = eliminate(A,b);
+		if( res != 0 ) {
+			fprintf( stderr, "[!] main.c: eliminacja nieudana, zwrocono kod %d\n", res );
+			return EXIT_FAILURE;
+		}
+		x = createMatrix(b->r, 1);
+		if (x != NULL) {
+			printf( "\nWyniki:\n");
+			printToScreen(A);
+			printToScreen(b);
+			backsubst(x,A,b);
+			printToScreen(x);
+			freeMatrix(x);
+		} else {
+			fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+		}
 	} else {
-		fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+		printf( "PASSED - wczytywanie danych - Macierz A [%dx%d], macierz b [%dx%d]\n", A->r, A->c, b->r, b->c );
+		
+		res = eliminate(A,b);
+		if( res != 0 ) {
+			fprintf( stderr, "[!] FAILED main.c: eliminacja nieudana, zwrocono kod %d\n", res );
+			return EXIT_FAILURE;
+		}
+		x = createMatrix(b->r, 1);
+		if ( NULL == x) {
+			fprintf( stderr,"[!] FAILED main.c: Nie mogłem utworzyć wektora wynikowego x\n" );
+			return EXIT_FAILURE;
+		}
+		res = backsubst(x,A,b);
+		if ( res != 0 ) {
+			fprintf( stderr, "[!] FAILED main.c: Nie udalo sie wykonac podstawienia\n" );
+			return EXIT_FAILURE;
+		}
+
+		freeMatrix(x);
 	}
 
 	freeMatrix(A);
